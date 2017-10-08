291bba39d0c186e6aacf0b9303bad23e9ebeec9a
Henry <th.saravana@gmail.com>
2017-10-08 19:47:08 +0530

    Initial Commit

---
+++ sample/src/main/java/com/madrapps/floatingactionmenu/FloatingActionMenu.kt
@@ -0,0 +1,13 @@
+package com.madrapps.floatingactionmenu
+
+import android.content.Context
+import android.util.AttributeSet
+import android.widget.RelativeLayout
+
+class FloatingActionMenu @JvmOverloads constructor(
+        context: Context,
+        attrs: AttributeSet? = null,
+        defStyleAttr: Int = 0
+) : RelativeLayout(context, attrs, defStyleAttr) {
+
+}
