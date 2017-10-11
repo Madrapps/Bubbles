0f6c2dac22a0a4eb889e6ad087a0c542721c9a8c
Henry <th.saravana@gmail.com>
2017-10-11 07:09:15 +0530

    #2 This is a crude way to code, but we did manage to replicate the animation. Now we have enough understanding to write tha API for animating the menu items.

--- sample/src/main/java/com/madrapps/floatingactionmenu/FloatingActionMenu.kt
+++ sample/src/main/java/com/madrapps/floatingactionmenu/FloatingActionMenu.kt
@@ -1,10 +1,13 @@
 package com.madrapps.floatingactionmenu
 
+import android.animation.AnimatorInflater
+import android.animation.AnimatorSet
 import android.content.Context
 import android.util.AttributeSet
 import android.view.View
 import android.view.ViewGroup
 import android.view.ViewGroup.LayoutParams.MATCH_PARENT
+import android.view.animation.DecelerateInterpolator
 
 class FloatingActionMenu @JvmOverloads constructor(
         context: Context,
@@ -12,6 +15,32 @@ class FloatingActionMenu @JvmOverloads constructor(
         defStyleAttr: Int = 0
 ) : ViewGroup(context, attrs, defStyleAttr) {
 
+    private var openAnimator = AnimatorInflater.loadAnimator(context, R.animator.open) as AnimatorSet
+    private var openAnimator1 = AnimatorInflater.loadAnimator(context, R.animator.open) as AnimatorSet
+    private var openAnimator2 = AnimatorInflater.loadAnimator(context, R.animator.open) as AnimatorSet
+    private var openAnimator3 = AnimatorInflater.loadAnimator(context, R.animator.open) as AnimatorSet
+    private var closeAnimator = AnimatorInflater.loadAnimator(context, R.animator.close) as AnimatorSet
+    private var closeAnimator1 = AnimatorInflater.loadAnimator(context, R.animator.close) as AnimatorSet
+    private var closeAnimator2 = AnimatorInflater.loadAnimator(context, R.animator.close) as AnimatorSet
+    private var closeAnimator3 = AnimatorInflater.loadAnimator(context, R.animator.close) as AnimatorSet
+
+    init {
+        openAnimator1.startDelay = 30
+        openAnimator2.startDelay = 60
+        openAnimator3.startDelay = 90
+        openAnimator.interpolator = DecelerateInterpolator()
+        openAnimator1.interpolator = DecelerateInterpolator()
+        openAnimator2.interpolator = DecelerateInterpolator()
+        openAnimator3.interpolator = DecelerateInterpolator()
+
+        closeAnimator.startDelay = 90
+        closeAnimator1.startDelay = 60
+        closeAnimator2.startDelay = 30
+        closeAnimator.interpolator = DecelerateInterpolator()
+        closeAnimator1.interpolator = DecelerateInterpolator()
+        closeAnimator2.interpolator = DecelerateInterpolator()
+        closeAnimator3.interpolator = DecelerateInterpolator()
+    }
 
     override fun onLayout(changed: Boolean, l: Int, t: Int, r: Int, b: Int) {
         val left = paddingLeft
@@ -36,6 +65,15 @@ class FloatingActionMenu @JvmOverloads constructor(
 
                     top = childBottom + lp.bottomMargin
                 }
+
+        openAnimator.setTarget(getChildAt(0))
+        openAnimator1.setTarget(getChildAt(1))
+        openAnimator2.setTarget(getChildAt(2))
+        openAnimator3.setTarget(getChildAt(3))
+        closeAnimator.setTarget(getChildAt(0))
+        closeAnimator1.setTarget(getChildAt(1))
+        closeAnimator2.setTarget(getChildAt(2))
+        closeAnimator3.setTarget(getChildAt(3))
     }
 
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
@@ -46,7 +84,6 @@ class FloatingActionMenu @JvmOverloads constructor(
                 .map { getChildAt(it) }
                 .filter { it.visibility != GONE }
                 .forEach { child ->
-
                     measureChildWithMargins(child, widthMeasureSpec, 0, heightMeasureSpec, 0)
 
                     val lp: MarginLayoutParams = child.layoutParams as MarginLayoutParams
@@ -65,4 +102,19 @@ class FloatingActionMenu @JvmOverloads constructor(
     override fun checkLayoutParams(p: LayoutParams?) = p is MarginLayoutParams
     override fun generateLayoutParams(p: LayoutParams?) = MarginLayoutParams(p)
     override fun shouldDelayChildPressedState() = false
+
+    fun open() {
+        openAnimator.start()
+        openAnimator1.start()
+        openAnimator2.start()
+        openAnimator3.start()
+    }
+
+    fun close() {
+        closeAnimator.start()
+        closeAnimator1.start()
+        closeAnimator2.start()
+        closeAnimator3.start()
+
+    }
 }
--- sample/src/main/java/com/madrapps/floatingactionmenu/MainActivity.kt
+++ sample/src/main/java/com/madrapps/floatingactionmenu/MainActivity.kt
@@ -1,12 +1,25 @@
 package com.madrapps.floatingactionmenu
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val menu = findViewById<FloatingActionMenu>(R.id.menuLayout)
+        var flag = true
+        findViewById<View>(R.id.floatingActionButton).setOnClickListener({
+            if (flag) {
+                menu.open()
+                flag = false
+            } else {
+                menu.close()
+                flag = true
+            }
+        })
     }
 }
